2b369ace7d3e0c5fc8cb5c705218212bdded33e8
혜민\forbe <hyeomin9326@gmail.com>
2023-01-26 23:13:30 +0900

    #회원 목록 조회 기능
    GetMapping으로 통일(미확정)

--- src/main/java/clonecoding/tinder/members/service/MembersService.java
+++ src/main/java/clonecoding/tinder/members/service/MembersService.java
@@ -29,14 +29,15 @@ public class MembersService {
     private final MemberRepository memberRepository;
     private final LikeRepository likeRepository;
 
-    @PostConstruct
+    //초기데이터 todo 삭제할 것
+//    @PostConstruct
     public void init() {
-        Member member1 = new Member("member1", "011", "pass", 126.925205, 37.4787760, "930206", "img1"); //소산
-        Member member2 = new Member("member2", "012", "pass", 126.923300, 37.5818396, "000123", "img1"); //보노
-        Member member3 = new Member("member3", "013", "pass", 126.729566, 37.4928588, "980703", "img1"); //인천
-        Member member4 = new Member("member4", "014", "pass", 127.028230, 37.5007549, "650403", "img1"); //강남
-        Member member5 = new Member("member5", "015", "pass", 126.925386, 37.4788392, "850519", "img1"); //문성로
-        Member member6 = new Member("member6", "016", "pass", 126.917397, 37.4749234, "990818", "img1"); //난곡로
+        Member member1 = new Member("member1", "011", "pass", 126.925205, 37.4787760, "930206", "https://cdn.pixabay.com/photo/2017/08/06/12/52/woman-2592247_960_720.jpg"); //소산
+        Member member2 = new Member("member2", "012", "pass", 126.923300, 37.5818396, "000123", "https://cdn.pixabay.com/photo/2017/08/06/12/52/woman-2592247_960_720.jpg"); //보노
+        Member member3 = new Member("member3", "013", "pass", 126.729566, 37.4928588, "980703", "https://cdn.pixabay.com/photo/2017/08/06/12/52/woman-2592247_960_720.jpg"); //인천
+        Member member4 = new Member("member4", "014", "pass", 127.028230, 37.5007549, "650403", "https://cdn.pixabay.com/photo/2017/08/06/12/52/woman-2592247_960_720.jpg"); //강남
+        Member member5 = new Member("member5", "015", "pass", 126.925386, 37.4788392, "850519", "https://cdn.pixabay.com/photo/2017/08/06/12/52/woman-2592247_960_720.jpg"); //문성로
+        Member member6 = new Member("member6", "016", "pass", 126.917397, 37.4749234, "990818", "https://cdn.pixabay.com/photo/2017/08/06/12/52/woman-2592247_960_720.jpg"); //난곡로
 
         memberRepository.save(member1);
         memberRepository.save(member2);
@@ -49,6 +50,12 @@ public class MembersService {
         likeRepository.save(likes);
     }
 
+    //API가 호출 될 때마다 회원 한 명씩 반환하기 위한 List
+    public static List<MembersResponseDto> membersResponseDtoList = new ArrayList<>();
+    public static int count = 0;
+    public static Map<Long, MembersResponseDto> map = new HashMap<>(); //좋아요 한 회원 삭제하는 용도
+
+    //회원 페이징하여 조회하기
     public Page<MembersResponseDto> getMembers(Pageable pageable, String phoneNum) {
 
         Member my = memberRepository.findByPhoneNum(phoneNum).orElseThrow(() -> new IllegalArgumentException("로그인을 해주세요"));
@@ -61,7 +68,55 @@ public class MembersService {
         Stream<MembersResponseDto> dtoList = members.stream().map(MembersResponseDto::fromEntity);
 
         //dto 안에서 거리를 계산하고 거리 순으로 정렬한다
-        return new PageImpl<>(dtoList.map(membersResponseDto ->
+        return new PageImpl<>(sortDto(my, dtoList));
+    }
+
+
+
+
+    public MembersResponseDto getMember(String phoneNum, MemberFindRequestDto requestDto) {
+
+        Member my = memberRepository.findByPhoneNum(phoneNum).orElseThrow(() -> new IllegalArgumentException("로그인을 해주세요"));
+
+        //좋아요를 눌렀다면
+        if (requestDto.isLike()) {
+            //좋아요 테이블에 좋아요 기록 저장
+            Likes likes = new Likes(requestDto.getId(), my.getId());
+            likeRepository.save(likes);
+            map.remove(requestDto.getId()); //map 에서 좋아요 한 사람 삭제하기
+        }
+
+        // 전체 회원 한 바퀴 다 돌았다면
+        if (count == membersResponseDtoList.size()) {
+
+            //dtoList 초기화하고 갱신된 map의 회원정보를 dto에 담아줌
+            membersResponseDtoList = new ArrayList<>();
+            for (int i = 0; i < map.size(); i++) {
+                membersResponseDtoList.add(map.get((long) i));
+            }
+            count = 0; //다시 0부터 카운트 시작
+        }
+
+        //전체 회원 중 이미 좋아요한 회원 제외하고 가져오기
+        //파라미터 (내 아이디, 페이지번호, 페이지 사이즈)
+        List<Member> members = memberRepository.findAllWithoutPaging(my.getId());
+
+        //entity -> dto 변환
+        Stream<MembersResponseDto> dtoList = members.stream().map(MembersResponseDto::fromEntity);
+
+        //dto 안에서 거리를 계산하고 거리 순으로 정렬한다
+        membersResponseDtoList = sortDto(my, dtoList);
+
+        //dto의 값들을 map에 넣어준다 (나중에 좋아요한 사람의 키 값을 가지고 삭제하기 위함)
+        membersResponseDtoList.stream().map(membersResponseDto -> map.put(membersResponseDto.getId(), membersResponseDto));
+
+        //api 호출 시마다 count++하여 dto에서 순차적으로 회원을 조회한다
+        return membersResponseDtoList.get(count++);
+    }
+
+    //dto에 거리와 나이를 계산하여 저장하고 거리순으로 정렬함
+    private List<MembersResponseDto> sortDto(Member my, Stream<MembersResponseDto> dtoList) {
+        return dtoList.map(membersResponseDto ->
                         MembersResponseDto.builder()
                                 .id(membersResponseDto.getId())
                                 .nickName(membersResponseDto.getNickName())
@@ -73,63 +128,8 @@ public class MembersService {
                                 .age(calculateAge(membersResponseDto.getBirthDate()))
                                 .build())
                 .sorted(Comparator.comparing(MembersResponseDto::getDistance))
-                .collect(Collectors.toList()));
-    }
-
-    //API가 호출 될 때마다 회원 한 명씩 반환하기 위한 List
-    public static List<MembersResponseDto> membersResponseDtoList = new ArrayList<>();
-
-    public static int count = 0;
-    public static Map<Long, MembersResponseDto> map = new HashMap<>(); //좋아요 시 삭제하
-
-    public MembersResponseDto getMember(String phoneNum, MemberFindRequestDto requestDto) {
-
-
-        Member my = memberRepository.findByPhoneNum(phoneNum).orElseThrow(() -> new IllegalArgumentException("로그인을 해주세요"));
-
-        //좋아요를 눌렀다면
-        if (requestDto.isLike()) {
-            //좋아요 테이블에 좋아요 기록 저장
-            Likes likes = new Likes(requestDto.getId(), my.getId());
-            likeRepository.save(likes);
-
-            //todo hashmap에서 삭제하기
-        }
-
-        if (count == membersResponseDtoList.size()) {
-            count = 0;
-        }
-
-        //전체 회원 중 이미 좋아요한 회원 제외하고 가져오기
-        //파라미터 (내 아이디, 페이지번호, 페이지 사이즈)
-        List<Member> members = memberRepository.findAllWithoutPaging(my.getId());
-
-        //entity -> dto 변환
-        Stream<MembersResponseDto> dtoList = members.stream().map(MembersResponseDto::fromEntity);
-
-
-
-        //dto 안에서 거리를 계산하고 거리 순으로 정렬한다
-        membersResponseDtoList = dtoList.map(membersResponseDto ->
-                        MembersResponseDto.builder()
-                                .id(membersResponseDto.getId())
-                                .nickName(membersResponseDto.getNickName())
-                                .birthDate(membersResponseDto.getBirthDate())
-                                .profile(membersResponseDto.getProfile())
-                                .distance(
-                                        calculateDistance(my.getLatitude(), my.getLongitude(), membersResponseDto.getLatitude(), membersResponseDto.getLongitude())
-                                )
-                                .age(calculateAge(membersResponseDto.getBirthDate()))
-                                .build())
-                .sorted(Comparator.comparing(membersResponseDto1 -> membersResponseDto1 != null ? membersResponseDto1.getDistance() : 0))
                 .collect(Collectors.toList());
-
-        membersResponseDtoList.stream().map(membersResponseDto -> map.put(membersResponseDto.getId(), membersResponseDto));
-
-        return membersResponseDtoList.get(count++);
     }
-
-
 
     //생년월일에서 나이 가져오기
     private int calculateAge(String birthDate) {
@@ -141,8 +141,8 @@ public class MembersService {
         }
         return (LocalDateTime.now().getYear() - year + 1);
     }
-    // Haversine formula (위도, 경도로 거리 구하기)
 
+    // Haversine formula (위도, 경도로 거리 구하기)
     private double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
         lat1 = Math.toRadians(lat1);
         lon1 = Math.toRadians(lon1);
