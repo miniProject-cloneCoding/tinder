4b58e98864e895f0301235d5d78bf5dc4ec76c94
sangheon1204 <77846519+sangheon1204@users.noreply.github.com>
2023-01-26 23:22:00 +0900

    Revert "좋아요 취소하기, 좋아요 누른 사람들 목록 조회"

--- src/main/java/clonecoding/tinder/member/repository/MemberRepository.java
+++ src/main/java/clonecoding/tinder/member/repository/MemberRepository.java
@@ -1,12 +1,7 @@
 package clonecoding.tinder.member.repository;
 
 import clonecoding.tinder.member.entity.Member;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface MemberRepository extends JpaRepository<Member, Long>, CustomMemberRepository {
-    Optional<Member> findByPhoneNum(String phoneNum);
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
