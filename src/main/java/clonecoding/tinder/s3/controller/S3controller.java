eb5430fdc99a7fb22be1b436e0046aeabd13f296
hyeunseungYu <hyeunseung03@gmail.com>
2023-01-27 02:13:44 +0900

    [#7]s3와 서버 연동
    파일 확장자 어떻게 제한할 것인지, 파일 크기 어떻게 제한할 것인지에 대한 논의가 필요합니다.

---
+++ src/main/java/clonecoding/tinder/s3/controller/S3controller.java
@@ -0,0 +1,34 @@
+package clonecoding.tinder.s3.controller;
+
+import clonecoding.tinder.s3.service.S3uploader;
+import clonecoding.tinder.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@RequiredArgsConstructor
+@RestController
+@Slf4j
+public class S3controller {
+    private final S3uploader s3uploader;
+
+    @PostMapping("/upload")
+    public String uploadFile(
+            @RequestParam("category") String category,
+            //multiPartFile을 사용하기 위해 @RequestPart를 사용하였습니다
+            //@RequestBody가 필요하지만 Binary Stream(바이너리 파일을 입출력 하는 것!
+            // 바이너리 파일은 텍스트가 아닌 실행 파일이나 그림 파일)이 포함되는 경우(MultipartFile과 같은)에 사용할 수 있다고 합니다.
+            @RequestPart(value = "file") MultipartFile multipartFile) throws IOException {
+        return s3uploader.upload(category, multipartFile);
+    }
+}
