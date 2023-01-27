7c9231ee04045b1d199b93ce523a311344d9619e
hyeunseungYu <hyeunseung03@gmail.com>
2023-01-27 09:41:44 +0900

    [#17] s3upload s3service로 이름 변경

--- src/main/java/clonecoding/tinder/s3/controller/S3controller.java
+++ src/main/java/clonecoding/tinder/s3/controller/S3controller.java
@@ -1,26 +1,21 @@
 package clonecoding.tinder.s3.controller;
 
-import clonecoding.tinder.s3.service.S3uploader;
-import clonecoding.tinder.security.UserDetailsImpl;
+import clonecoding.tinder.s3.service.S3service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @RequiredArgsConstructor
 @RestController
 @Slf4j
 public class S3controller {
-    private final S3uploader s3uploader;
+    private final S3service s3Service;
 
     @PostMapping("/upload")
     public String uploadFile(
@@ -29,6 +24,6 @@ public class S3controller {
             //@RequestBody가 필요하지만 Binary Stream(바이너리 파일을 입출력 하는 것!
             // 바이너리 파일은 텍스트가 아닌 실행 파일이나 그림 파일)이 포함되는 경우(MultipartFile과 같은)에 사용할 수 있다고 합니다.
             @RequestPart(value = "file") MultipartFile multipartFile) throws IOException {
-        return s3uploader.upload(category, multipartFile);
+        return s3Service.upload(category, multipartFile);
     }
 }
