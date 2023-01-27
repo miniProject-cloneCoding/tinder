bdbbc68edbbf4a5647f913a48d443392f6602abb
혜민\forbe <hyeomin9326@gmail.com>
2023-01-27 14:41:23 +0900

    #swagger config 수정
    basic-error-controller 안보이게 함

--- src/main/java/clonecoding/tinder/config/SwaggerConfig.java
+++ src/main/java/clonecoding/tinder/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package clonecoding.tinder.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,12 +15,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .ignoredParameterTypes(AuthenticationPrincipal.class)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
