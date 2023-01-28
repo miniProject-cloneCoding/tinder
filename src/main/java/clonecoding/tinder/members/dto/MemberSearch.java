df22567b84e08e9a313f2c6e0761f9b6fcb8191e
혜민\forbe <hyeomin9326@gmail.com>
2023-01-28 20:08:09 +0900

    #1 회원가입 수정 - 원하는 성별 데이터타입 변경

--- src/main/java/clonecoding/tinder/members/dto/MemberSearch.java
+++ src/main/java/clonecoding/tinder/members/dto/MemberSearch.java
@@ -14,16 +14,8 @@ public class MemberSearch {
     private boolean female;
     private boolean male;
 
-    public MemberSearch(String[] wantedGender) {
-        if (wantedGender.length == 2) { // 두 성별 모두 원하는 경우
-            female = true;
-            male = true;
-        } else if (wantedGender[0].equals("0")) { //여자만 원하는 경우
-            female = true;
-            male = false;
-        } else {  //남자만 원하는 경우
-            female = false;
-            male = true;
-        }
+    public MemberSearch(boolean male, boolean female) {
+        this.male = male;
+        this.female = female;
     }
 }
